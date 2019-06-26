f19f2e4bdf95056fd5c98400e7c4687df976f86e
Captainfive <vinou_montes@hotmail.fr>
2019-06-26 12:12:44 +0200

    chore(index): creating addon and main function

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
+// Require Node.js Dependencies
+const { readFile } = require("fs").promises;
+const { join } = require("path");
+
+// Require utils functions
+const { ConfigFsc } = require("./src/utils");
+
+// Require Slimio Dependencies
 const Addon = require("@slimio/addon");
 
-const FSC = new Addon("FSC");
+// Create addon FSC
+const FSC = new Addon("FSC", {
+    version: "0.1.0",
+    verbose: true
 
+}).lockOn("events");
 
 FSC.on("start", () => {
     FSC.ready();
+    async function main() {
+        const json = await readFile(join(process.cwd(), "config.json"));
+        const jsonParsed = JSON.parse(json);
+
+        for (const [profileName, profile] of Object.entries(jsonParsed.profiles)) {
+            const { target, rules } = profile;
+            const classTool = new ConfigFsc(target, rules);
+            if (!profile.active) {
+                continue;
+            }
+            await classTool.checkRules();
+        }
+    }
+    main().catch(console.error);
+});
+
+FSC.on("stop", () => {
+    console.log("addon stopped");
 });
 
 module.exports = FSC;

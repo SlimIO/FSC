07e83b00aa02d43ad4a982ab7b0b0afb7a62259c
Captainfive <vinou_montes@hotmail.fr>
2019-07-17 17:17:51 +0200

    feat(index): adding hot reload on multiple profiles

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 // Require Node.js Dependencies
-const { readFile, access } = require("fs").promises;
+const { access, stat } = require("fs").promises;
 const { join } = require("path");
 
 const { entityAge, filesNumber, repositoryNumber,
     dirSize, readTime, integrity, spaceOfTarget } = require("./src/utils");
 
-// const checkRules = require("./src/utils");
-
 // Require Slimio Dependencies
-// const Addon = require("@slimio/addon");
-// const alert = require("@slimio/alert");
-// const metrics = require("@slimio/metrics");
+const Config = require("@slimio/config");
+const Addon = require("@slimio/addon");
+const alert = require("@slimio/alert");
+const metrics = require("@slimio/metrics");
+const Scheduler = require("@slimio/scheduler");
 
-// // Create addon FSC
-// const FSC = new Addon("FSC", {
-//     version: "0.1.0",
-//     verbose: true
+// Create addon FSC
+const FSC = new Addon("FSC", {
+    version: "0.1.0",
+    verbose: true
+}).lockOn("events");
+const { Entity } = metrics(FSC);
+const { Alarm } = alert(FSC);
 
-// }).lockOn("events");
-// const { Entity } = metrics(FSC);
-// const { Alarm } = alert(FSC);
+let intervalId;
 
-// let intervalId;
-// const MyEntity = new Entity("MyEntity", {
-//     description: "Hello world!"
-// });
+// Déclare entities and MIC
+const MyEntity = new Entity("MyEntity", {
+    description: "Central metricsing"
+});
+
 
 /**
  * @version 0.1.0
@@ -33,107 +35,169 @@ const { entityAge, filesNumber, repositoryNumber,
  * @param {Array<String>} rules profile
  * @param {!String} target location
  * @param {!String} name profile
+ * @param {!Boolean} metrics profile
  * @desc Check the active rules of a profile and send an alarm when the rules are reached.
  * @return {Promise<void>}
  */
-async function checkRules(rules, target, name) {
+// eslint-disable-next-line
+async function checkRules(rules, target, name, metrics) {
     for (const rule of rules) {
+        const st = await stat(target);
         switch (rule.name) {
             case "age_limiter":
-                if (await entityAge(target) > rule.value) {
+            {
+                const it = await entityAge(target, name);
+                if (it > rule.value) {
+                    console.log(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`);
                     // new Alarm(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`, {
                     //     correlateKey: "age_limiter_alarm",
                     //     entity: MyEntity
                     // });
                 }
                 break;
+            }
             case "files_number":
-                if (await filesNumber(target, name) > rule.value) {
+            {
+                const it = await filesNumber(target, name);
+                if (st.isFile()) {
+                    // new Alarm(`${name} : ${rule.name} target must be a path to a repository`, {
+                    //     correlateKey: "files_number_alarm",
+                    //     entity: MyEntity
+                    // });
+                    break;
+                }
+                else if (it > rule.value) {
+                    console.log(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`);
                     // new Alarm(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`, {
                     //     correlateKey: "files_number_alarm",
                     //     entity: MyEntity
                     // });
                 }
                 break;
+            }
             case "repository_number":
-                if (await repositoryNumber(target, name) > rule.value) {
+            {
+                const it = await repositoryNumber(target, name);
+                if (st.isFile()) {
+                    // new Alarm(`${name} : ${rule.name} target must be a path to a repository`, {
+                    //     correlateKey: "repository_number_alarm",
+                    //     entity: MyEntity
+                    // });
+                    break;
+                }
+                else if (it > rule.value) {
+                    console.log(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`);
                     // new Alarm(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`, {
-                    //     correlateKey: "files_number_alarm",
+                    //     correlateKey: "repository_number_alarm",
                     //     entity: MyEntity
                     // });
                 }
                 break;
+            }
             case "size_limiter":
-                if (await dirSize(target) > rule.value) {
+            {
+                const it = await dirSize(target);
+                if (it > rule.value) {
+                    console.log(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`);
                     // new Alarm(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`, {
                     //     correlateKey: "size_limiter_alarm",
                     //     entity: MyEntity
                     // });
                 }
                 break;
+            }
             case "space_limiter":
-                if (await spaceOfTarget(target) > rule.value) {
+            {
+                const it = await spaceOfTarget(target);
+                if (it > rule.value) {
+                    console.log(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`);
                     // new Alarm(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`, {
                     //     correlateKey: "size_limiter_alarm",
                     //     entity: MyEntity
                     // });
                 }
                 break;
+            }
             case "read_time":
-                if (await readTime(target) > rule.value) {
+            {
+                const it = await readTime(target);
+                if (st.isDirectory()) {
+                    // new Alarm(`${name} : ${rule.name} target must be a path to a repository`, {
+                    //     correlateKey: "readtime_alarm",
+                    //     entity: MyEntity
+                    // });
+                    break;
+                }
+                else if (it > rule.value) {
+                    console.log(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`);
                     // new Alarm(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`, {
-                    //     correlateKey: "read_time_alarm",
+                    //     correlateKey: "readtime_alarm",
                     //     entity: MyEntity
                     // });
                 }
                 break;
+            }
             case "integrity":
-                if (await integrity(target) > rule.value) {
-                    // new Alarm(`${name} : ${rule.name} | reached | ${it} expected => ${rule.value}`, {
+            {
+                const it = await integrity(target);
+                if (st.isDirectory()) {
+                    // new Alarm(`${name} : ${rule.name} target must be a path to a repository`, {
                     //     correlateKey: "integrity_alarm",
                     //     entity: MyEntity
                     // });
+                    break;
                 }
                 break;
+            }
         }
     }
 }
 
-// FSC.on("awake", () => {
-//     FSC.ready();
-async function main() {
-    // Read config.json
-    const json = await readFile(join(process.cwd(), "config.json"));
-    // Parse config.json
-    const { profiles } = JSON.parse(json);
-    // Check all rules of all profiles
-    for (const [profileName, profile] of Object.entries(profiles)) {
-        const { target, interval, rules } = profile;
-        if (!profile.active) {
-            continue;
+FSC.on("awake", async() => {
+    cfg = new Config(join(__dirname, "config.json"), {
+        autoReload: true,
+        writeOnSet: true
+    });
+    await cfg.read();
+    const intervalTest = await Promise.all(
+        Object.values(cfg.payload.profiles).map((profile) => profile)
+    );
+
+    let arr = [];
+    cfg.observableOf("profiles").subscribe({
+        next() {
+            arr = [];
+            for (let id = 0; id < intervalTest.length; id++) {
+                intervalTest[id].interval = new Scheduler({ interval: Number(`${cfg.get(`profiles.${intervalTest[id].name}.interval`)}`) });
+                arr.push(intervalTest[id]);
+            }
+        },
+        error(err) {
+            console.error(err);
         }
-        intervalId = setInterval(async() => {
-            try {
-                await access(target);
-                await checkRules(rules, target, profileName);
+    });
+    setInterval(async() => {
+        for (const profile of arr) {
+            if (!profile.interval.walk()) {
+                continue;
             }
-            catch (error) {
-                console.log(error);
-                // new Alarm(`${profileName} : ${profileName} doesn't exist !`, {
-                //     correlateKey: "access_alarm",
-                //     entity: MyEntity
-                // });
-            }
-        }, interval);
-    }
-}
-main().catch(console.error);
+            await access(profile.target);
+            await checkRules(profile.rules, profile.target, profile.name, profile.metrics);
+        }
+    }, 100);
+    await FSC.ready();
+});
+
+FSC.on("close", async() => {
+    clearInterval(intervalId);
+    await cfg.close();
+    console.log("addon stopped");
+});
+
+
+// FSC.registerCallback(async function main() {
+//     console.log("hello world");
 // });
+// FSC.schedule("main", new Scheduler({ interval: 1 }));
 
-// FSC.on("close", () => {
-//     clearInterval(intervalId);
-//     console.log("addon stopped");
-
-// });
-
-// module.exports = FSC;
+module.exports = FSC;
